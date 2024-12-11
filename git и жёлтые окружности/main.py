aca933ea9fd510c9498a0db1a228c54f1be56e7d
Амелин Михаил Викторович <ks123amelin@oat.local>
2024-12-11 16:45:38 +0600

    Окружности

---
+++ git и жёлтые окружности/main.py
@@ -0,0 +1,43 @@
+import sys
+import random
+from PyQt5 import QtWidgets, uic, QtGui
+
+class CircleApp(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(CircleApp, self).__init__()
+        uic.loadUi('UI.ui', self)
+
+        # Подключаем кнопку к методу
+        self.pushButton.clicked.connect(self.add_circle)
+
+        # Создаем область для рисования
+        self.canvas = QtWidgets.QLabel(self)
+        self.canvas.setGeometry(10, 50, 480, 320)  # Устанавливаем размер области
+        self.canvas.setStyleSheet("background-color: white;")
+        self.canvas.setPixmap(QtGui.QPixmap(self.canvas.size()))
+        self.canvas.pixmap().fill(QtGui.QColor("white"))
+
+    def add_circle(self):
+        # Получаем размеры области рисования
+        width = self.canvas.width()
+        height = self.canvas.height()
+
+        # Генерируем случайный диаметр и позицию окружности
+        diameter = random.randint(20, 100)
+        x = random.randint(0, width - diameter)
+        y = random.randint(0, height - diameter)
+
+        # Рисуем окружность
+        painter = QtGui.QPainter(self.canvas.pixmap())
+        painter.setBrush(QtGui.QColor("yellow"))
+        painter.drawEllipse(x, y, diameter, diameter)
+        painter.end()
+
+        # Обновляем виджет для отображения изменений
+        self.canvas.update()
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = CircleApp()
+    window.show()
+    sys.exit(app.exec_())
